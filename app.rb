1c3b711a0beb53ab16da535176445603997ca020
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-01-21 15:53:51 +0000

    Shotgun gem installed and session_secret set

--- app.rb
+++ app.rb
@@ -1,5 +1,15 @@
 require 'sinatra'
+require 'shotgun'
+set :session_secret, 'super secret'
 
 get '/' do
-  "Hello!"
+  "Hello World!"
+end
+
+get '/secret' do
+  'Working!'
+end
+
+get '/home' do
+  "I'm home"
 end
