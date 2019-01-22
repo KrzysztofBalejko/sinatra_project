7ceed9d6d3fa9e9ae42a70e28b555528fc1b7c18
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-01-22 12:46:34 +0000

    Creating post request and extracting form to dedicated file

--- app.rb
+++ app.rb
@@ -2,24 +2,28 @@ require 'sinatra'
 require 'shotgun'
 set :session_secret, 'super secret'
 
-get '/' do
-  "Hello World!"
+# get '/' do
+#   "Hello World!"
+# end
+#
+# get '/secret' do
+#   'Working!'
+# end
+#
+# get '/home' do
+#   "I'm home"
+# end
+
+# get '/random-cat' do
+#   @name = ["Amigo", "Oscar", "Viking"].sample
+#   erb(:index)
+# end
+
+get '/cat-form' do
+  erb :cat_form
 end
 
-get '/secret' do
-  'Working!'
-end
-
-get '/home' do
-  "I'm home"
-end
-
-get '/random-cat' do
-  @name = ["Amigo", "Oscar", "Viking"].sample
-  erb(:index)
-end
-
-get '/named-cat' do
+post '/named-cat' do
   p params
   @name = params[:name]
   erb(:index)
